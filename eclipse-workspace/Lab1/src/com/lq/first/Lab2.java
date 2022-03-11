e82205c5c37dd4ee6b022b44c66ce659bbaaf67d
filesForClass <101416713+filesForClass@users.noreply.github.com>
2022-03-11 08:29:55 -0800

    Add files via upload

---
+++ eclipse-workspace/Lab1/src/com/lq/first/Lab2.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.lq.first;
+
+/**
+ * @author Student
+ *
+ */
+public class Lab2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		
+		int age;
+		
+		age = 10;
+		
+		int age_2 = 10;
+		
+		int age_3, age_4;
+		
+		System.out.println("I am: " + age);
+		
+		float f_1 = 45.67F;
+		
+
+	}
+
+}
